211ac179a02db2f3e7ac6064e85d3208b49ad799
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2023-12-28 13:08:26 +0300

    some routine updates

--- Assets/Time and Routine/Scripts/TimeManager.cs
+++ Assets/Time and Routine/Scripts/TimeManager.cs
@@ -15,8 +15,8 @@ public class TimeManager : MonoBehaviour
     private float timer;
     void Start()
     {
-        Minute = 0;
-        Hour = 0;
+        Minute = 20;
+        Hour = 15;
     }
 
     // Update is called once per frame
